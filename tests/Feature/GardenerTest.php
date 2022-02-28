fe0678b10871ee97fc443c7a6a7aa429a1ba0c45
dreywandowski <aduramimo@gmail.com>
2022-02-28 18:22:23 +0100

    test post endpoints_user_creation

--- tests/Feature/GardenerTest.php
+++ tests/Feature/GardenerTest.php
@@ -40,33 +40,6 @@ class GardenerTest extends TestCase
         $response->assertStatus(200);
     }
 
-    // test create gardener endpoint - 201 OK
-    public function test_create_gardener_created_endpoint()
-    {
-        $response = $this->post('/api/register', ['fullname' => 'Sally Gardener',
-            'email' => 'feature2@test.com',
-            'password' => '!@#980gff33',
-            'location' => 'Nairobi',
-            'country' => 'Kenya',
-            'isCustomer' => false
-        ]);
-
-        $response->assertStatus(201);
-    }
-
-    // test create gardener endpoint -- 400 Failed
-    public function test_create_gardener_failed_endpoint()
-    {
-        $response = $this->post('/api/register', ['fullname' => 'Sally Gardener',
-             'email' => 'feature2@test.com',
-            'password' => '!@#980gff33',
-            'location' => 'Nairobi',
-            'country' => 'Kenya',
-            'isCustomer' => false
-        ]);
-
-        $response->assertStatus(400);
-    }
 
     // test create customer endpoint -- 201 OK
     public function test_create_customer_created_endpoint()
@@ -78,14 +51,6 @@ class GardenerTest extends TestCase
                                                       'country' => 'Nigeria',
                                                        'isCustomer' => true
                                                            ]);
-        $this->assertJson([
-            'fullname' => ['The name field is required.'],
-            'email' => ['The email field is required.'],
-            'password' => ['The password field is required.'],
-            'location' => ['The location field is required.'],
-            'country' => ['The country field is required.'],
-            'isCustomer' => ['the isCustomer field is required'],
-        ]);
 
 
         $response->assertStatus(201);
