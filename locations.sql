6999fa986a3678bc20325fe3675956876a4417d8
dreywandowski <aduramimo@gmail.com>
2022-02-28 13:57:04 +0100

    update sql

--- locations.sql
+++ locations.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Feb 27, 2022 at 06:33 PM
--- Server version: 8.0.18
--- PHP Version: 7.4.0
+-- Host: localhost:3306
+-- Generation Time: Feb 28, 2022 at 12:59 PM
+-- Server version: 8.0.28-0ubuntu0.20.04.3
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,17 +28,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `locations`
 --
 
-DROP TABLE IF EXISTS `locations`;
-CREATE TABLE IF NOT EXISTS `locations` (
-  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `locations` (
+  `id` bigint UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `location` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `country` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `locations_location_foreign` (`location`),
-  KEY `locations_country_foreign` (`country`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `location` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `country` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `locations`
@@ -48,7 +44,29 @@ INSERT INTO `locations` (`id`, `created_at`, `updated_at`, `location`, `country`
 (1, '2022-02-26 15:04:18', NULL, 'Lagos', 'Nigeria'),
 (2, '2022-02-26 15:04:15', NULL, 'Nairobi', 'Kenya'),
 (3, '2022-02-26 15:04:18', NULL, 'Ibadan', 'Nigeria'),
-(4, '2022-02-26 15:06:15', NULL, 'Mombasa', 'Kenya');
+(4, '2022-02-26 15:06:15', NULL, 'Mombassa', 'Kenya');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `locations`
+--
+ALTER TABLE `locations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `locations_location_foreign` (`location`),
+  ADD KEY `locations_country_foreign` (`country`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `locations`
+--
+ALTER TABLE `locations`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
